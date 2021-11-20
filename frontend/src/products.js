04c5c1475e1f4fa2148bf57074bacaced9078e4b
Ivan <ivang12@g.ucla.edu>
2021-11-20 09:46:48 -0800

    load images via frontend for testing purposes

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'UCLA Sweater',
+    image: '/images/ucla_sweater.jpg',
+    description:
+      'UCLA sweater, not only to keep you warm, but also to add some style to your wardrobe.',
+    brand: 'UCLA',
+    category: 'Clothing',
+    price: 54.99,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 10,
+  },
+  {
+    _id: '2',
+    name: 'UCLA Keychain',
+    image: '/images/keychain.jpg',
+    description:
+      'A chain to hang on your backpack or on other accessories. Style your life.',
+    brand: 'UCLA',
+    category: 'Misc.',
+    price: 7.99,
+    countInStock: 15,
+    rating: 5,
+    numReviews: 15,
+  },
+  {
+    _id: '3',
+    name: 'UCLA Thermos',
+    image: '/images/thermos.jpg',
+    description:
+      'A UCLA Thermos for drinking.',
+    brand: 'Hydroflask',
+    category: 'Misc.',
+    price: 49.99,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 8,
+  },
+  {
+    _id: '4',
+    name: 'UCLA Hat',
+    image: '/images/ucla_hat.jpg',
+    description:
+      'The ultimate hat to attract more people',
+    brand: 'UCLA',
+    category: 'Clothing',
+    price: 25.99,
+    countInStock: 25,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'UCLA Tshirt',
+    image: '/images/ucla_tshirt.jpg',
+    description:
+      'A UCLA Tshirt to better your experience in the outside world',
+    brand: 'UCLA',
+    category: 'Clothing',
+    price: 18.99,
+    countInStock: 34,
+    rating: 4,
+    numReviews: 15,
+  },
+  {
+    _id: '6',
+    name: 'UCLA Sweatpants',
+    image: '/images/ucla_sweatpants.jpg',
+    description:
+      'Sweatpants to style your life and keep your leg warm',
+    brand: 'UCLA',
+    category: 'Clothing',
+    price: 29.99,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 13,
+  },
+]
+
+export default products
